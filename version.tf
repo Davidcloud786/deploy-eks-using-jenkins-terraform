b969bfe7cacdbcd061dee22e1e6a2f4b6d87dc77
Davidcloud786 <david.globalcloud@gmail.com>
2025-12-24 04:57:17 +0000

    Update provider versions in version.tf

--- version.tf
+++ version.tf
@@ -4,11 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 6.0"
     }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.25"
+      version = ">= 3.0"
     }
   }
 }
